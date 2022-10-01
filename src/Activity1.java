9542d435b1ee129696604217193c7b5edbb8ce3d
shannen <shannenmendoza.310@gmail.com>
2022-10-01 13:55:18 +0800

    add quotation mark and dot on display output

--- src/Activity1.java
+++ src/Activity1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Activity1 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         // This program will print your name
 
         String firstName;
@@ -10,17 +10,17 @@ public class Activity1 {
         String lastName;
 
 
-        System.out.println("Enter Your  First Name: ");
+        System.out.print("Enter Your  First Name: ");
         firstName = sc.nextLine();
 
-        System.out.println("Enter Your  Middle Initial: ");
-        middleInitial=sc.nextLine();
+        System.out.print("Enter Your  Middle Initial: ");
+        middleInitial = sc.nextLine();
 
-        System.out.println("Enter Your  Last Name: ");
+        System.out.print("Enter Your  Last Name: ");
         lastName = sc.nextLine();
 
 
-        System.out.println("Your name is " +  firstName + middleInitial + lastName);
+        System.out.println("Your name is " + firstName + " " + middleInitial + " " + lastName + ".");
+
     }
 }
-
